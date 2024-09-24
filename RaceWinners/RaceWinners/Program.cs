7d42f91d6a659d1afeabe28e8f0fa72f4ed3b5f4
NonOxC <59771975+NonOxC@users.noreply.github.com>
2024-09-24 02:54:44 -0400

    Stuff

--- RaceWinners/RaceWinners/Program.cs
+++ RaceWinners/RaceWinners/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace RaceWinners;
 
@@ -11,13 +12,15 @@ public class Program
 
         // Asynchronously retrieve the group (class) data
         var data = await ds.GetGroupRanksAsync();
-
+        var min = data.OrderBy(i => i.Ranks.Average()).First();
         for (int i = 0; i < data.Count; i++)
         {
             // Combine the ranks to print as a list
             var ranks = String.Join(", ", data[i].Ranks);
-            
+
             Console.WriteLine($"{data[i].Name} - [{ranks}]");
+
         }
+        Console.WriteLine($"The winner is: {min.Name}");
     }
 }
